811fae62082a5a1cd1b83e253d006b99d40092a9
HibiKier <775757368@qq.com>
2025-06-13 16:42:23 +0800

    :sparkles: 添加GitCode同步工作流和更新插件仓库地址

--- zhenxun/builtin_plugins/plugin_store/config.py
+++ zhenxun/builtin_plugins/plugin_store/config.py
@@ -9,3 +9,6 @@ DEFAULT_GITHUB_URL = "https://github.com/zhenxun-org/zhenxun_bot_plugins/tree/ma
 
 EXTRA_GITHUB_URL = "https://github.com/zhenxun-org/zhenxun_bot_plugins_index/tree/index"
 """插件库索引github仓库地址"""
+
+DEFAULT_GITCODE_RAW_URL = "https://raw.gitcode.com/gh_mirrors/zh/zhenxun_bot/raw/main"
+"""伴生插件gitcode仓库地址"""
