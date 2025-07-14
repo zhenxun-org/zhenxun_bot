36316e21ecce1c072145675f5c81c1e828058ff4
HibiKier <775757368@qq.com>
2025-07-14 22:54:35 +0800

    🔧 移除db_url参数以简化数据库配置获取逻辑

--- zhenxun/services/db_context.py
+++ zhenxun/services/db_context.py
@@ -357,7 +357,7 @@ SQLITE_CONFIG = {
 }
 
 
-def get_config(db_url: str) -> dict:
+def get_config() -> dict:
     """获取数据库配置"""
     parsed = urlparse(BotConfig.db_url)
 
@@ -426,7 +426,7 @@ async def init():
         raise DbUrlIsNode("\n" + error.strip())
     try:
         await Tortoise.init(
-            config=get_config(BotConfig.db_url),
+            config=get_config(),
         )
         if SCRIPT_METHOD:
             db = Tortoise.get_connection("default")
